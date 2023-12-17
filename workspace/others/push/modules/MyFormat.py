da6fb53ec767f57984a835367806c77df233a35d
VuVanNghia <nghiavu2k2abc@gmail.com>
2023-12-18 00:08:42 +0700

    VuVanNghia20206205

--- workspace/others/push/modules/MyFormat.py
+++ workspace/others/push/modules/MyFormat.py
@@ -1,8 +1,26 @@
+import os
+import glob
+
+
 class MyFormat:
-    def workspace(path):
-        with open(path, 'r', encoding="utf-8") as file:
+    def workspace(file):
+        with open(file, 'r', encoding="utf-8") as file:
             contents = file.read()
         while '  ' in contents:
             contents = contents.replace('  ', ' ')
-        with open(path, 'w', encoding="utf-8") as file:
+        with open(file, 'w', encoding="utf-8") as file:
             file.write(contents)
+
+    def markdown(path):
+        files = glob.glob(os.path.join(path, f'**/*.md'), recursive=True)
+        for file in files:
+            with open(file, 'r', encoding="utf-8") as file:
+                contents = file.read()
+            while '  ' in contents:
+                contents = contents.replace('  ', ' ')
+            contents = '\n'.join(line.strip() for line in contents.split('\n'))
+            while "\n\n\n" in contents:
+                contents = contents.replace("\n\n\n", "\n\n")
+            contents = contents.lstrip('\n')
+            with open(file, 'w', encoding="utf-8") as file:
+                file.write(contents) 
