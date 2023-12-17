49aa2048303f01097d3d8d87d91ec99d070ad109
VuVanNghia <nghiavu2k2abc@gmail.com>
2023-12-18 00:52:16 +0700

    VuVanNghia20206205

--- workspace/others/push/modules/MyFormat.py
+++ workspace/others/push/modules/MyFormat.py
@@ -12,6 +12,10 @@ class MyFormat:
             contents = file.read()
         while '  ' in contents:
             contents = contents.replace('  ', ' ')
+        contents = '\n'.join(line.strip() for line in contents.split('\n'))
+        while "\n\n\n" in contents:
+            contents = contents.replace("\n\n\n", "\n\n")
+        contents = contents.lstrip('\n')
         with open(workspace_path, 'w', encoding="utf-8") as file:
             file.write(contents)
 
