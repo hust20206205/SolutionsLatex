75a1408a6add49a9a8312a2feb9579f9cf93b0cc
VuVanNghia <nghiavu2k2abc@gmail.com>
2023-12-18 00:14:24 +0700

    VuVanNghia20206205

--- workspace/others/push/modules/MyFormat.py
+++ workspace/others/push/modules/MyFormat.py
@@ -23,18 +23,33 @@ class MyFormat:
                 contents = contents.replace("\n\n\n", "\n\n")
             contents = contents.lstrip('\n')
             with open(file_markdown, 'w', encoding="utf-8") as file_markdown:
-                file_markdown.write(contents) 
+                file_markdown.write(contents)
 
     def latex(git_path):
-        files = glob.glob(os.path.join(git_path, f'**/*.md'), recursive=True)
+        files = glob.glob(os.path.join(git_path, f'**/*.tex'), recursive=True)
         for file_latex in files:
             with open(file_latex, 'r', encoding="utf-8") as file_latex:
                 contents = file_latex.read()
+
             while '  ' in contents:
                 contents = contents.replace('  ', ' ')
+
+            while '( ' in contents:
+                contents = contents.replace('( ', '(')
+            while ' )' in contents:
+                contents = contents.replace(' )', ')')
+            while '[ ' in contents:
+                contents = contents.replace('[ ', '[')
+            while ' ]' in contents:
+                contents = contents.replace(' ]', ']')
+            while '{ ' in contents:
+                contents = contents.replace('{ ', '{')
+            while ' }' in contents:
+                contents = contents.replace(' }', '}')
+
             contents = '\n'.join(line.strip() for line in contents.split('\n'))
             while "\n\n\n" in contents:
                 contents = contents.replace("\n\n\n", "\n\n")
             contents = contents.lstrip('\n')
             with open(file_latex, 'w', encoding="utf-8") as file_latex:
-                file_latex.write(contents) 
+                file_latex.write(contents)
