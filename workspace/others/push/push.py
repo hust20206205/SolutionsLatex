3a541e171a10b10a495490b23f6d796f9a8e701e
VuVanNghia <nghiavu2k2abc@gmail.com>
2023-12-17 23:49:31 +0700

    VuVanNghia20206205

--- workspace/others/push/push.py
+++ workspace/others/push/push.py
@@ -3,11 +3,8 @@ from modules.MyFormat import MyFormat
 import os
 
 
-# vi_tri=os.getcwd()
-# print("🐍 File: push/push.py | Line: 7 | undefined ~ vi_tri",vi_tri)
 git_path=os.path.join(os.getcwd(),'../../../')
 workspace_path=os.path.join(os.getcwd(),'../../vvn20206205.code-workspace')
-# print("🐍 File: push/push.py | Line: 7 | undefined ~ vi_tri",vi_tri)
 
 message = "VuVanNghia20206205"
 MyGit.chdir(git_path)
