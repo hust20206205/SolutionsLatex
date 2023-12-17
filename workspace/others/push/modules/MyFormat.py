e45acd9da9b5ced31e79414d59adfd7391f9b171
VuVanNghia <nghiavu2k2abc@gmail.com>
2023-12-18 00:10:34 +0700

    VuVanNghia20206205

--- workspace/others/push/modules/MyFormat.py
+++ workspace/others/push/modules/MyFormat.py
@@ -3,24 +3,24 @@ import glob
 
 
 class MyFormat:
-    def workspace(file):
-        with open(file, 'r', encoding="utf-8") as file:
+    def workspace(workspace_path):
+        with open(workspace_path, 'r', encoding="utf-8") as file:
             contents = file.read()
         while '  ' in contents:
             contents = contents.replace('  ', ' ')
-        with open(file, 'w', encoding="utf-8") as file:
+        with open(workspace_path, 'w', encoding="utf-8") as file:
             file.write(contents)
 
-    def markdown(path):
-        files = glob.glob(os.path.join(path, f'**/*.md'), recursive=True)
-        for file in files:
-            with open(file, 'r', encoding="utf-8") as file:
-                contents = file.read()
+    def markdown(git_path):
+        files = glob.glob(os.path.join(git_path, f'**/*.md'), recursive=True)
+        for file_markdown in files:
+            with open(file_markdown, 'r', encoding="utf-8") as file_markdown:
+                contents = file_markdown.read()
             while '  ' in contents:
                 contents = contents.replace('  ', ' ')
             contents = '\n'.join(line.strip() for line in contents.split('\n'))
             while "\n\n\n" in contents:
                 contents = contents.replace("\n\n\n", "\n\n")
             contents = contents.lstrip('\n')
-            with open(file, 'w', encoding="utf-8") as file:
-                file.write(contents) 
+            with open(file_markdown, 'w', encoding="utf-8") as file_markdown:
+                file_markdown.write(contents) 
