9d43ca12e2039346522569bd8b48ad930d64aeac
VuVanNghia <nghiavu2k2abc@gmail.com>
2023-12-17 23:48:50 +0700

    VuVanNghia20206205

--- workspace/others/push/modules/MyFormat.py
+++ workspace/others/push/modules/MyFormat.py
@@ -1,5 +1,8 @@
 class MyFormat:
     def workspace(path):
-        with open(path, 'r') as file: 
-            content = file.read()
-        print(content)
+        with open(path, 'r', encoding="utf-8") as file:
+            contents = file.read()
+        while '  ' in contents:
+            contents = contents.replace('  ', ' ')
+        with open(path, 'w', encoding="utf-8") as file:
+            file.write(contents)
