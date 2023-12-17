73dfc6f85d9eec8d61c6aa74e68c56c52057053f
VuVanNghia <nghiavu2k2abc@gmail.com>
2023-12-17 23:56:44 +0700

    VuVanNghia20206205

--- workspace/others/push/push.py
+++ workspace/others/push/push.py
@@ -1,13 +1,28 @@
+import os
 from modules.MyGit import MyGit
 from modules.MyFormat import MyFormat
-import os
+from modules.MyView import MyView
 
 
 git_path=os.path.join(os.getcwd(),'../../../')
 workspace_path=os.path.join(os.getcwd(),'../../vvn20206205.code-workspace')
 
 message = "VuVanNghia20206205"
+
 MyGit.chdir(git_path)
 MyGit.add()
 MyGit.commit(message)
+
 MyFormat.workspace(workspace_path)
+
+# MyView.CloseTab()
+# MyView.Target(2)
+# MyView.CloseTerminal()
+# MyView.CloseScrollBar()
+# MyView.CollapseFolders()
+# MyView.OpenGit()
+# MyView.OpenLatex()
+
+
+
+
